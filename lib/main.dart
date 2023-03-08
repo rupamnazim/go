93a8aa8426d083a9233c13069b59dbfb4ef7a0b5
rupam0 <rupamnazim0@gmail.com>
2023-03-08 11:16:32 +0600

    Park5

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,66 @@
 import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
-// import 'package:flutter/services.dart';
+// import 'package:flutter/rendering.dart';
 
-void main() => runApp(const MyApp());
+// ignore_for_file: unused_local_variable
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+void main() => runApp(const CoolApp());
+
+class CoolApp extends StatelessWidget {
+  const CoolApp({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const greet = Text('Hello');
     return MaterialApp(
-        theme: ThemeData.dark(),
-        home: Scaffold(
-            appBar: AppBar(
-                leading: const Icon(Icons.menu),
-                backgroundColor: Colors.red,
-                title: const Center(
-                    child: Text('CoolApp',
-                        style: TextStyle(
-                            color: Color.fromARGB(255, 88, 255, 82))))),
-            body: const Center(
-              child: Text(
-                'Let\'s go',
-                style: TextStyle(
-                    fontSize: 50,
-                    color: Color.fromARGB(255, 74, 255, 86),
-                    backgroundColor: Colors.red),
-              ),
-            )));
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('CoolColors'),
+          leading: const Icon(Icons.home),
+          backgroundColor: Colors.indigo,
+        ),
+//Body starts from here...
+        body: const Center(
+          child: Text(
+            'Slide left to open drawer.',
+            style: TextStyle(
+                fontSize: 30,
+                color: Color.fromARGB(255, 255, 255, 255),
+                backgroundColor: Colors.red),
+          ),
+        ),
+
+//drawer starts from here...
+        drawer: Drawer(
+            backgroundColor: Colors.white60,
+            child: ListView(
+// ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const ListTile(
+// ignore: unnecessary_const
+                  leading: const Icon(
+                    Icons.account_circle,
+                    size: 30,
+                  ),
+// ignore: unnecessary_const
+                  title: const Text(
+                    'Profile',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+                const ListTile(
+                  leading: Icon(
+                    Icons.settings,
+                    size: 30,
+                  ),
+                  title: Text(
+                    'Settings',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ],
+            )),
+      ),
+    );
   }
 }
